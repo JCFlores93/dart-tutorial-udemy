ec15d014a9dd8655355fd74594725f8619331e6a
Jean Carlo Flores Carrasco <jeancarlo_flores93@hotmail.com>
2018-08-02 21:07:59 -0500

    first commit

---
+++ layout/intro_layouts_containers/lib/ui/home.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      color: Colors.greenAccent,
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("First item",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            color: Colors.white
+          ),
+          ),
+          Text("second Item",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.blue)
+          ),
+          Container(
+            color: Colors.deepOrange.shade50,
+            alignment: Alignment.bottomLeft,
+            child: Text("Third item",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 10.1),)
+          )
+        ],
+      ),
+//      child: Text(
+//        "Hello container",
+//        textDirection: TextDirection.ltr,
+//        style: new TextStyle(
+//            color: Colors.white,
+//            fontWeight: FontWeight.w900,
+//            fontSize: 18.3
+//        ),
+//      ),
+    );
+  }
+}
