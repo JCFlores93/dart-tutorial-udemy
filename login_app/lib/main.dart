f960a77182fe46f0af450a036d8f51dd679b48af
Jean Carlo Flores Carrasco <jeancarlo_flores93@hotmail.com>
2018-08-12 20:29:13 -0500

    state - stateful

---
+++ login_app/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import './ui/Login.dart';
+void main() {
+  runApp(MaterialApp(
+    title: "Login",
+    home: Login()
+  ));
+}
---
+++ login_app/lib/ui/Login.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LoginState();
+  }
+}
+
+class LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      backgroundColor: Colors.blueGrey,
+    );
+  }
+
+}
